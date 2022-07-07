bad1484276dc05e9fe935c8a4bda4ba649771465
christian-cell <cristohp74@gmail.com>
2022-07-07 15:11:43 +0200

    termino el crud con todas las operaciones con DJANGO

--- src/api/views.py
+++ src/api/views.py
@@ -13,31 +13,51 @@ class ArticulosView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    
 
-    def get(self,request,id=0):
-        if(id>0):
+    def get(self, request, id=0):
+        if (id > 0):
             articulos = list(Articulos.objects.filter(id=id).values())
             if len(articulos) > 0:
                 articulo = articulos[0]
-                datos={'message':'Success','articulos':articulo}
+                datos = {'message': "Success", 'articulo': articulo}
             else:
-                datos={'message':'no se encontró el articulo por id'}
+                datos = {'message': "Articulo not found..."}
             return JsonResponse(datos)
         else:
-            articulos=list(Articulos.objects.values())
-            if len(articulos)>0:
-                datos={'message':'Success','articulos':articulos}
+            articulos = list(Articulos.objects.values())
+            if len(articulos) > 0:
+                datos = {'message': "Success", 'articulos': articulos}
             else:
-                datos={'message':"No hay articulos en la tabla"}
+                datos = {'message': "Article not found..."}
             return JsonResponse(datos)
-    def post(self,request):
-       
+
+    def post(self, request):
+        # print(request.body)
         jd = json.loads(request.body)
-        Articulos.objects.create(name=jd['name'],maker_website=jd['maker_website'],made_at=jd['made_at'])
-        datos =  {'message':'Success'}
+        # print(jd)
+        Articulos.objects.create(name=jd['name'], maker_website=jd['maker_website'], made_at=jd['made_at'])
+        datos = {'message': "Success"}
         return JsonResponse(datos)
-    def put(self,request):
-        pass
-    def delete(self,request):
-        pass
+
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        articulos = list(Articulos.objects.filter(id=id).values())
+        if len(articulos) > 0:
+            articulo = Articulos.objects.get(id=id)
+            articulo.name = jd['name']
+            articulo.maker_website = jd['maker_website']
+            articulo.made_at = jd['made_at']
+            articulo.save()
+            datos = {'message': "Success"}
+        else:
+            datos = {'message': "Article not found..."}
+        return JsonResponse(datos)
+
+    def delete(self, request, id):
+        articulos = list(Articulos.objects.filter(id=id).values())
+        if len(articulos) > 0:
+            Articulos.objects.filter(id=id).delete()
+            datos = {'message': "Success"}
+        else:
+            datos = {'message': "Article not found..."}
+        return JsonResponse(datos)
