8c5d10a84e6681d1dd731b19ac3daebfcbcca949
christian-cell <cristohp74@gmail.com>
2022-07-07 14:14:43 +0200

    añado la ruta post y la ruta de get por id

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import ArticulosView
 
 urlpatterns = [
-    path('articulos/' , ArticulosView.as_view() , name='lista_de_articulos')
+    path('articulos/' , ArticulosView.as_view() , name='lista_de_articulos'),
+    path('articulos/<int:id>' , ArticulosView.as_view() , name='articulos_por_id')
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -3,20 +3,40 @@ from django.shortcuts import render
 from django.views import View
 from .models import Articulos
 from django.http.response import JsonResponse
-
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 #poo
 
 class ArticulosView(View):
-    def get(self,request):
-        articulos=list(Articulos.objects.values())
-        if len(articulos)>0:
-            datos={'message':'Success','articulos':articulos}
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get(self,request,id=0):
+        if(id>0):
+            articulos = list(Articulos.objects.filter(id=id).values())
+            if len(articulos) > 0:
+                articulo = articulos[0]
+                datos={'message':'Success','articulos':articulo}
+            else:
+                datos={'message':'no se encontró el articulo por id'}
+            return JsonResponse(datos)
         else:
-            datos={'message':"No hay articulos en la tabla"}
-        return JsonResponse(datos)
+            articulos=list(Articulos.objects.values())
+            if len(articulos)>0:
+                datos={'message':'Success','articulos':articulos}
+            else:
+                datos={'message':"No hay articulos en la tabla"}
+            return JsonResponse(datos)
     def post(self,request):
-        pass
+       
+        jd = json.loads(request.body)
+        Articulos.objects.create(name=jd['name'],maker_website=jd['maker_website'],made_at=jd['made_at'])
+        datos =  {'message':'Success'}
+        return JsonResponse(datos)
     def put(self,request):
         pass
     def delete(self,request):
